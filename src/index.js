7ec365a40d7d8e2bd7fe47620f81d27027e94bce
Ilya Kreymer <ikreymer@gmail.com>
2022-11-13 09:39:47 -0800

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { concat, getSize, makeDir, addToDir } from "./concat.js";
 export { splitAdd, parseSplits, iterSegments } from "./split-add.js";
 export { traverse } from "./traverse.js";
+export { BlockFS } from "./store.js";
